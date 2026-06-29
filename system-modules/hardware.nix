7ce6d4d7e105a830b57a0e293e6419f7e8b20630
Keio <keiyo.medias@gmail.com>
2026-06-29 20:17:55 +0800

    Fixed oversteer support

--- system-modules/hardware.nix
+++ system-modules/hardware.nix
@@ -20,5 +20,6 @@
     # Logitech Steering Wheel
     hardware.new-lg4ff.enable = true;
     services.udev.packages = with pkgs; [ oversteer ];
+    environment.systemPackages = with pkgs; [ oversteer ];
   };
 }
