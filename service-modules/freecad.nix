6c65eec980831e781a9951a9b02f009237d2c82f
Keio <keiyo.medias@gmail.com>
2026-07-21 21:59:16 +0800

    Fixed FreeCAD crashing

--- service-modules/freecad.nix
+++ service-modules/freecad.nix
@@ -1,6 +1,13 @@
 { config, pkgs, lib, ... }:
 {
   environment.systemPackages = with pkgs; [
-    freecad
+    (symlinkJoin {
+      name = "freecad-xwayland";
+      paths = [ freecad ];
+      buildInputs = [ makeWrapper ];
+      postBuild = ''
+        wrapProgram $out/bin/freecad --unset XDG_SESSION_TYPE
+      '';
+    })
   ];
 }
